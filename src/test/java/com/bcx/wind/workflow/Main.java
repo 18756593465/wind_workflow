0c6a25dd798515117cb6beeb3327ceb146b6eafb
18756593465 <2538970366@qq.com>
2019-02-21 19:24:47 +0800

    提交测试代码

--- src/test/java/com/bcx/wind/workflow/Main.java
+++ src/test/java/com/bcx/wind/workflow/Main.java
@@ -27,6 +27,7 @@ public class Main {
 
         System.out.println();
         System.out.println();
+        System.out.println(); System.out.println();
         System.out.println();
 
         System.out.println();
